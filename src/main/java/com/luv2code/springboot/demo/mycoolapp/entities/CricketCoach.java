0287b33452ac7bedcd35e2479fc52ceff3ce9d8c
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-18 11:47:05 -0300

    Bean Lifecycle methods

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/entities/CricketCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/entities/CricketCoach.java
@@ -1,14 +1,12 @@
 package com.luv2code.springboot.demo.mycoolapp.entities;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.luv2code.springboot.demo.mycoolapp.entities.interfaces.Coach;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // This will create a new instance every time
+// @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // This will create a new instance every time
 @Primary
 public class CricketCoach implements Coach{
 
--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -5,19 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springboot.demo.mycoolapp.entities.interfaces.Coach;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @RestController
 public class DemoController {
     
     // define a private field for the dependecy
     private Coach myCoach;
-    private Coach anotherCoach;
 
     // define a constructor for the dependecy injection
     // @Autowired (Optional if there is only one constructor)
-    public DemoController(Coach theCoach, Coach theAnotherCoach){
+    public DemoController(Coach theCoach){
         System.out.println("In constructor:" + this.getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
 
     // define a setter method for the dependecy injection
@@ -26,13 +27,22 @@ public class DemoController {
     //     myCoach = theCoach;
     // }
 
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In init method:" + this.getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In destroy method:" + this.getClass().getSimpleName());
+    }
+
+
     @GetMapping("/dailyworkout")
     public String getDailyWorlout(){
         return myCoach.getDailyWorkout();
     }
 
-    @GetMapping("/check")
-    public String check(){
-        return "Comparing Beans: my coach == another coach: " + (myCoach == anotherCoach);
-    }
 }
