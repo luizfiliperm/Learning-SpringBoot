01748197e8a7240097adcfb4d52fc6701dfe704b
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-18 18:59:03 -0300

    Setting up Spring Boot project - Database Acess Hibernate/JPA CRUD

--- src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.demo.mycoolapp;
+package com.luv2code.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MycoolappApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
