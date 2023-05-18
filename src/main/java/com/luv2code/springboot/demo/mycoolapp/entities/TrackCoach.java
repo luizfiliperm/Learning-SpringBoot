4381c63084da0edfcba81de62585c89aa7605097
Luiz Filipe <luizfilipemr17@gmail.com>
2023-05-18 09:55:21 -0300

    Bean Scopes

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/entities/TrackCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/entities/TrackCoach.java
@@ -1,12 +1,10 @@
 package com.luv2code.springboot.demo.mycoolapp.entities;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.luv2code.springboot.demo.mycoolapp.entities.interfaces.Coach;
 
 @Component
-@Primary
 public class TrackCoach implements Coach{
 
     public TrackCoach(){
--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/DemoController.java
@@ -10,12 +10,14 @@ public class DemoController {
     
     // define a private field for the dependecy
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // define a constructor for the dependecy injection
     // @Autowired (Optional if there is only one constructor)
-    public DemoController(Coach theCoach){
+    public DemoController(Coach theCoach, Coach theAnotherCoach){
         System.out.println("In constructor:" + this.getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     // define a setter method for the dependecy injection
@@ -27,5 +29,10 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorlout(){
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "Comparing Beans: my coach == another coach: " + (myCoach == anotherCoach);
     }
 }
