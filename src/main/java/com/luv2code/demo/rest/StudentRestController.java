eeaaa1eb3b9d5539fa7d29a62da166db838d011b
luizfiliperm <luizfilipemr17@gmail.com>
2023-06-12 17:52:01 -0300

    Global Exception Handling

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -3,9 +3,6 @@ package com.luv2code.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -60,40 +57,5 @@ public class StudentRestController {
         return students.get(studentId);
     }
 
-    // add an exception handler using @ExceptionHandler
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-
-    // add another exception handler ... to catch any exception (catch all)
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-
-        // create a StudentErrorResponse
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        // return ResponseEntity
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
+    
 }
