37ee3c2354100242a58908560b16fdd4f6406ead
luizfiliperm <luizfilipemr17@gmail.com>
2023-06-12 14:35:44 -0300

    Path Variables

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.demo.entities.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("api")
 public class StudentRestController {
+
+    private List<Student> students;
+
+    // define @PostConstruct to load the sutdent data ... only once
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
 
     // define endpoint for "/students" - return list of students
 
@@ -26,4 +42,13 @@ public class StudentRestController {
         return students;
     }
 
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // index into the list
+
+        return students.get(studentId);
+    }
 }
