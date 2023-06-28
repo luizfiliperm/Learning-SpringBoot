0632685a03b63a3befc27064bd0d15341d633658
luizfiliperm <luizfilipemr17@gmail.com>
2023-06-28 09:51:46 -0300

    Adding Spring Data Rest Configs

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.cruddemo.entities.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
