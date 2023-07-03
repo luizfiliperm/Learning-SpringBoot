62f4d320186ca9572c3490b7876f6c645c254e77
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-03 18:07:29 -0300

    Restrict URLs based on roles

--- src/main/java/com/luv2code/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,5 +34,25 @@ public class DemoSecurityConfig {
             .build();
 
         return new InMemoryUserDetailsManager(jhon, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+            configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
