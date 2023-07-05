e7ad2968a3620a7df5eae52fdb28570d8c212d8d
luizfiliperm <luizfilipemr17@gmail.com>
2023-07-05 09:47:45 -0300

    quickstart

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.demo.mycoolapp;
+package com.luv2code.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycoolappApplication {
+public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
+		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 }
